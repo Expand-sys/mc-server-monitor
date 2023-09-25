3bd14546e17e82319eeb60e5acb18d26e3b87db5
Expand <Expand@ur-mom.gay>
2023-09-25 17:32:07 +1000

    finished ish

--- index.ts
+++ index.ts
@@ -1,59 +1,71 @@
 console.log("starting monitor - use nohup to daemonize");
 const got = require("got");
-const Gamedig = require('gamedig');
+const mcs = require('node-mcstatus');
 
 let serverstatus = 0
 runevery15seconds(1)
 function runevery15seconds(i) {
     setTimeout(() => {
-        Gamedig.query({
-            type: 'minecraft',
-            host: 'mc.emre-personal.net',
-            port: 25565,
-            debug: true,
-            socketTimeout: 3000
-        }).then((state) => {
-            console.log(state);
-            if(serverstatus == 0){
-                var params = {
-                    username: "MC-monitor",
-                    avatar_url: "https://i.kym-cdn.com/photos/images/newsfeed/001/338/171/d12.jpg",
-                    content: "Server is back online",
-                }
 
-                fetch('https://discord.com/api/webhooks/1155727545724047400/IvcXVXZNpX10fwHhPaIZQ6OYl5XJ2f0iFoBhrYnKtGYHWR2_SMHruXyhcIZR8IsY8P8V', {
-                    "method":"POST",
-                    "headers": {"Content-Type": "application/json"},
-                    "body": JSON.stringify(params)
-                  })
-                    .then(res=> console.log(res))
-                    .catch(err => console.error(err));
-            }
-            serverstatus = 1
-        
-        }).catch((error: any) => {
-            console.log("Server is offline");
-            console.log(error)
-            if( serverstatus == 1){
+        const host = `${process.env.MCADDRESS}`;
+        const port = process.env.PORT;
+        const options = { query: true };
+
+        // The `port` argument is optional and defaults
+        // to 25565. The `options` argument is optional.
+        mcs.statusJava(host, port, options)
+            .then((result) => {
+                if(!result.online && serverstatus == 1){
+                    console.log("offline")
+                    var params = {
+                        username: "MC-monitor",
+                        avatar_url: "https://i.kym-cdn.com/photos/images/newsfeed/001/338/171/d12.jpg",
+                        content: "Server is Offline <@247288496442703872>",
+                    }
+
+                    fetch(`${process.env.WEBHOOK}`, {
+                        "method":"POST",
+                        "headers": {"Content-Type": "application/json"},
+                        "body": JSON.stringify(params)
+                      })
+                        .then(res=> console.log(res))
+                        .catch(err => console.error(err));
+                    serverstatus = 0
+                }else if(result.online && serverstatus == 0) {
+                    console.log("Online")
+                    var params = {
+                        username: "MC-monitor",
+                        avatar_url: "https://i.kym-cdn.com/photos/images/newsfeed/001/338/171/d12.jpg",
+                        content: "Server is back online",
+                    }
+    
+                    fetch(`${process.env.WEBHOOK}`, {
+                        "method":"POST",
+                        "headers": {"Content-Type": "application/json"},
+                        "body": JSON.stringify(params)
+                      })
+                        .then(res=> console.log(res))
+                        .catch(err => console.error(err));
+                    serverstatus = 1;
+                }
+            })
+            .catch((error) => {
                 var params = {
                     username: "MC-monitor",
                     avatar_url: "https://i.kym-cdn.com/photos/images/newsfeed/001/338/171/d12.jpg",
-                    content: "Server is Offline <@247288496442703872>",
+                    content: "Checking service is Offline",
                 }
                 console.log(error)
-                fetch('https://discord.com/api/webhooks/1155727545724047400/IvcXVXZNpX10fwHhPaIZQ6OYl5XJ2f0iFoBhrYnKtGYHWR2_SMHruXyhcIZR8IsY8P8V', {
+                fetch(`${process.env.WEBHOOK}`, {
                     "method":"POST",
                     "headers": {"Content-Type": "application/json"},
                     "body": JSON.stringify(params)
                   })
                     .then(res=> console.log(res))
                     .catch(err => console.error(err));
-            }
-            serverstatus = 0
-            
-        
-        });
-        runevery15seconds(++i);
-    }, 5000)
+            })
+            runevery15seconds(++i)
+    }, process.env.CHECK_TIMER)
+    
 }
 
